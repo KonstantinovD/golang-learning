c966aaeeb9ce8129c86acb16a2adc1169701c5df
konstantinov_daniil407@mail.ru <konstantinov_daniil407@mail.ru>
2022-01-16 15:17:01 +0300

    [go-website] filter POST requests using headers

--- GO_WEBSITE_01/main.go
+++ GO_WEBSITE_01/main.go
@@ -30,6 +30,45 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 
 // Обработчик для создания новой заметки.
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	// -- Используем метод Header().Set() для добавления заголовка
+	// 'Allow: POST' в map HTTP-headers, чтобы пользователь знал,
+	// какие HTTP-методы поддерживаются для определенного URL.
+	// -- Header().Set вызывается строго перед WriteHeader()/Write()
+	w.Header().Set("Allow", http.MethodPost)
+	// Используем r.Method для проверки, использует ли запрос метод POST
+	// или нет. Обратите внимание, что http.MethodPost является строкой
+	// и содержит текст "POST".
+	if r.Method != http.MethodPost {
+		// Если это не так, то вызывается метод w.WriteHeader() для
+		// возврата статус-кода 405 и вызывается метод w.Write() для
+		// тела-ответа с текстом "Метод запрещен".
+		w.WriteHeader(405)
+		// -- Вызвать метод w.WriteHeader() в обработчике можно только
+		// один раз, иначе Go выдаст сообщение об ошибке;
+		// -- Если не вызывать метод w.WriteHeader() напрямую, тогда
+		// первый вызов w.Write() автоматически отправит пользователю
+		// код состояния 200 OK. Поэтому, если вы хотите вернуть другой
+		// код состояния, вызовите один раз метод w.WriteHeader() перед
+		// любым вызовом w.Write().
+		w.Write([]byte("GET-Метод запрещен!"))
+		return
+	}
+
+	w.Write([]byte("Форма для создания новой заметки..."))
+}
+
+// Обработчик для создания пустой заметки.
+func createEmptySnippet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Allow", http.MethodPost)
+	if r.Method != http.MethodPost {
+		// Если требуется отправить какой-то код состояния, кроме 200
+		// с текстом ответа, можно использовать http.Error(). Эта
+		// функция принимает текст сообщения и код состояния, а затем
+		// сама вызывает методы w.WriteHeader() и w.Write()
+		http.Error(w, "Метод запрещен!", 405)
+		return
+	}
+
 	w.Write([]byte("Форма для создания новой заметки..."))
 }
 
@@ -47,6 +86,7 @@ func main() {
 	mux.HandleFunc("/", home)                        // non-fixed path (ends with '/')
 	mux.HandleFunc("/snippet", showSnippet)          // fixed path
 	mux.HandleFunc("/snippet/create", createSnippet) // fixed path
+	mux.HandleFunc("/snippet/empty/create", createEmptySnippet)
 
 	// Используется функция http.ListenAndServe() для запуска нового
 	// веб-сервера. Мы передаем два параметра: TCP-адрес сети для
